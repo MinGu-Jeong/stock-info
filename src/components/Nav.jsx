b1f62c5bfdc82f342463e077dc22dc0c3321e1e6
MinGu Jeong <mingu830@gmail.com>
2023-08-05 05:38:58 +0900

    feat: Nav 수정

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,44 @@
+import { useEffect, useState } from "react";
 import * as S from "./Nav.style";
-import Swiper from "swiper";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Nav() {
-  const date =
-    new Date().getMonth() + 1 + "월 " + new Date().getDate() + "일" + " 기준";
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [menu, setMenu] = useState("홈");
+  const [date, setDate] = useState("");
+  useEffect(() => {
+    setDate(
+      new Date().getMonth() + 1 + "월 " + new Date().getDate() + "일" + " 기준"
+    );
+  }, []);
+
+  useEffect(() => {
+    const currentPath = location.pathname;
+    if (currentPath === "/") {
+      setMenu("홈");
+    } else if (currentPath === "/news") {
+      setMenu("뉴스");
+    }
+  }, [location]);
+
   return (
     <S.Nav>
-      <S.NavTitle>타이틀 위치</S.NavTitle>
-      <S.NavMenuCurrent>홈</S.NavMenuCurrent>
+      <S.NavTitle></S.NavTitle>
+      {menu === "홈" ? (
+        <S.NavMenuCurrent onClick={() => navigate("/")}>홈</S.NavMenuCurrent>
+      ) : (
+        <S.NavMenu onClick={() => navigate("/")}>홈</S.NavMenu>
+      )}
       <S.NavMenu>주식</S.NavMenu>
       <S.NavMenu>etf</S.NavMenu>
-      <S.NavMenu>뉴스</S.NavMenu>
+      {menu === "뉴스" ? (
+        <S.NavMenuCurrent onClick={() => navigate("/news")}>
+          뉴스
+        </S.NavMenuCurrent>
+      ) : (
+        <S.NavMenu onClick={() => navigate("/news")}>뉴스</S.NavMenu>
+      )}
       <S.NavMenu>MY</S.NavMenu>
       <S.NavAlarmDateContainer>
         <S.NavAlarm>알람발생 위치</S.NavAlarm>
