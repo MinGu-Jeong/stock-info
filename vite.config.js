17c3db5b26f2d1d481c28d7b9dfa487801f557cc
MinGu Jeong <mingu830@gmail.com>
2023-08-05 05:38:41 +0900

    chore: 환경설정 수정

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://openapi.naver.com",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        ws: true,
+      },
+    },
+  },
 });
