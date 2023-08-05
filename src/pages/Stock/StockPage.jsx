dbd5fe169be739a03054cc2bb1996582a6b50ad5
MinGu Jeong <mingu830@gmail.com>
2023-08-06 05:38:03 +0900

    feat: 주식 상세 페이지

--- src/pages/Stock/StockPage.jsx
+++ src/pages/Stock/StockPage.jsx
@@ -1,3 +1,121 @@
+import { useEffect, useState } from "react";
+import * as S from "./StockPage.style";
+import * as N from "../News/News.style";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 export default function StockPage() {
-  return <div></div>;
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const stockId = params.get("id");
+  const [stockData, setStockData] = useState({});
+  useEffect(() => {
+    axios
+      .get(
+        `https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey=${
+          import.meta.env.VITE_REACT_APP_STOCK_PRICE_API_KEY
+        }&resultType=json&likeSrtnCd=${stockId}`
+      )
+      .then((res) => {
+        setStockData(res.data.response.body.items.item[0]);
+        let fltRt = parseFloat(res.data.response.body.items.item[0].fltRt);
+        fltRt = fltRt.toFixed(2);
+        res.data.response.body.items.item[0].fltRt =
+          fltRt > 0 ? `+${fltRt}` : `${fltRt}`;
+      });
+  }, []);
+  const [page, setPage] = useState(1);
+  const [newsData, setNewsData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("/api/v1/search/news", {
+        params: { query: `${stockData?.itmsNm}`, start: page, display: 5 },
+        headers: {
+          "X-Naver-Client-Id": import.meta.env.VITE_REACT_APP_NAVER_CLIENT_ID,
+          "X-Naver-Client-Secret": import.meta.env
+            .VITE_REACT_APP_NAVER_CLIENT_SECRET,
+        },
+      })
+      .then((res) => {
+        setNewsData(res.data.items);
+      });
+  }, [page, stockData]);
+  const handleNextPage = () => {
+    setPage(page + 5);
+  };
+  const handlePrevPage = () => {
+    setPage(page - 5);
+    if (page <= 1) setPage(1);
+  };
+  return (
+    <div>
+      <S.FlexColumn>
+        <S.ContentMargin>
+          <S.StockDetailContainer>
+            <S.StockDetailLeftWrapper>
+              <S.StockId>{stockId}</S.StockId>
+              <S.StockName>{stockData?.itmsNm}</S.StockName>
+              <S.StockPrice>{stockData?.clpr}</S.StockPrice>
+              <S.StockPercentChange isRising={stockData?.fltRt > 0}>
+                {stockData?.fltRt}%
+              </S.StockPercentChange>
+            </S.StockDetailLeftWrapper>
+            <S.StockDetailRightWrapper>
+              <S.StockDetailWrapper>
+                <S.StockInfo>전일</S.StockInfo>
+                <S.StockInfoValue>
+                  {stockData?.clpr - stockData?.vs}
+                </S.StockInfoValue>
+              </S.StockDetailWrapper>
+              <S.StockDetailWrapper>
+                <S.StockInfo>거래량(억)</S.StockInfo>
+                <S.StockInfoValue>
+                  {(parseInt(stockData?.mrktTotAmt) / 100000000).toFixed(0)}
+                </S.StockInfoValue>
+              </S.StockDetailWrapper>
+              <S.StockDetailWrapper>
+                <S.StockInfo>시가</S.StockInfo>
+                <S.StockInfoValue>{stockData?.mkp}</S.StockInfoValue>
+              </S.StockDetailWrapper>
+              <S.StockDetailWrapper>
+                <S.StockInfo>고가</S.StockInfo>
+                <S.StockInfoValue>{stockData?.hipr}</S.StockInfoValue>
+              </S.StockDetailWrapper>
+              <S.StockDetailWrapper>
+                <S.StockInfo>저가</S.StockInfo>
+                <S.StockInfoValue>{stockData.lopr}</S.StockInfoValue>
+              </S.StockDetailWrapper>
+            </S.StockDetailRightWrapper>
+          </S.StockDetailContainer>
+          <N.NewsContainer>
+            <h1>{stockData?.itmsNm} 종목뉴스</h1>
+            {newsData.map((news) => {
+              return (
+                <a
+                  key={news.link}
+                  href={news.link}
+                  target="_blank"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  <N.NewsWrapper>
+                    <h2 dangerouslySetInnerHTML={{ __html: news.title }} />
+                    <p dangerouslySetInnerHTML={{ __html: news.description }} />
+                  </N.NewsWrapper>
+                </a>
+              );
+            })}
+
+            <N.PageWrapper>
+              {page === 1 ? (
+                <N.PrevPageDisabled />
+              ) : (
+                <N.PrevPage onClick={handlePrevPage} />
+              )}
+              <N.PageNumber>{Math.round(page / 10) + 1}</N.PageNumber>
+              <N.NextPage onClick={handleNextPage} />
+            </N.PageWrapper>
+          </N.NewsContainer>
+        </S.ContentMargin>
+      </S.FlexColumn>
+    </div>
+  );
 }
---
+++ src/pages/Stock/StockPage.style.jsx
@@ -0,0 +1,106 @@
+import styled from "styled-components";
+
+export const FlexColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #f2f5f7;
+  height: auto;
+`;
+export const ContentMargin = styled.div`
+  padding-top: 50px;
+  margin-top: 50px;
+  width: 100vw;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+export const StockDetailContainer = styled.div`
+  width: 1050px;
+  height: auto;
+  margin-top: 50px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  background-color: #fff;
+  border-radius: 10px;
+  display: flex;
+`;
+export const StockDetailLeftWrapper = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 25px;
+`;
+export const StockDetailRightWrapper = styled.div`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 25px;
+`;
+export const StockDetailWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
+`;
+export const StockId = styled.div`
+  color: #000;
+  text-align: center;
+  font-family: Inter;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+export const StockName = styled.div`
+  color: #000;
+  text-align: center;
+  font-family: Inter;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+export const StockPrice = styled.div`
+  color: #000;
+  text-align: center;
+  font-family: Inter;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+export const StockPercentChange = styled.div`
+  color: ${(props) => (props.isRising ? "#da4841" : "#377BEB")};
+  text-align: center;
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+export const StockInfo = styled.div`
+  color: #000;
+  text-align: center;
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  width: 100px;
+`;
+export const StockInfoValue = styled.div`
+  color: #000;
+  text-align: center;
+  font-family: Inter;
+  font-size: 23px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
